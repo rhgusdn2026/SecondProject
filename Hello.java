2be7d5e2486b1baa23b41709e5fddff9fa7a16dc
gusdn2026 <gusdn2026@naver.com>
2016-07-07 15:23:08 +0900

    added output "Bitkeeper"

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello git");
+		System.out.println("what is Git?");
+		System.out.println("2002. BitKeeper")
 	}
 }
