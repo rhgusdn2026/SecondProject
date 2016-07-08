d9eae4dd22e1c5ca19c76f35f2af47b195091e2f
gusdn2026 <gusdn2026@naver.com>
2016-07-08 16:33:41 +0900

    conflict resolved

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello git");
 		System.out.println("what is Git?");
+		System.out.println("That's DVCS");
+		System.our.println("bitKeeper");
 		System.out.println("2002. BitKeeper");
 	}
 }
