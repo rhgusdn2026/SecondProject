e7d95e9c2cc4e963da728350c0bbc13229250827
rhgusdn2026 <gusdn2026@naver.com>
2016-07-20 15:16:58 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,7 +5,6 @@ public class Hello{
 		System.out.println("Hello git");
 		System.out.println("what is Git?");
 		System.out.println("That's DVCS");
-		System.our.println("bitKeeper");
 		System.out.println("2002. BitKeeper");
 	}
 }
