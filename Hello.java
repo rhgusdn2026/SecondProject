536d12c0b4545c4702d6f6aee3fb6b004672d886
rhgusdn2026 <gusdn2026@naver.com>
2016-07-20 15:17:26 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@
  //GitHub modification
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello git");
+		System.out.println("Hello Git!");
 		System.out.println("what is Git?");
 		System.out.println("That's DVCS");
 		System.out.println("2002. BitKeeper");
