3e84def338a89d5efb17af6b9434150036b1b1c2
gusdn2026 <gusdn2026@naver.com>
2016-07-20 15:24:30 +0900

    Hello.java modified on Locla repository

--- Hello.java
+++ Hello.java
@@ -1,9 +1,10 @@
+//For command line git tutorial
+//Local repository modification
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello git");
+		System.out.println("Hello git!");
 		System.out.println("what is Git?");
 		System.out.println("That's DVCS");
-		System.our.println("bitKeeper");
 		System.out.println("2002. BitKeeper");
 	}
 }
